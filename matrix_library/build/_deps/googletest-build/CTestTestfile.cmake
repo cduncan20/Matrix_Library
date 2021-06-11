aaadb9b3ca3150e70a6c60f418aa806462cee8c1
Casey <56935554+cduncan20@users.noreply.github.com>
2021-06-10 21:28:33 -0600

    Delete matrix_library/build directory
    This was added on accident.

--- matrix_library/build/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/casey/repos/duncan_casey/matrix_library/build/_deps/googletest-src
-# Build directory: /home/casey/repos/duncan_casey/matrix_library/build/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- matrix_library/build/test/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/casey/repos/duncan_casey/matrix_library/test
-# Build directory: /home/casey/repos/duncan_casey/matrix_library/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/home/casey/repos/duncan_casey/matrix_library/build/test/matrix_library_test[1]_include.cmake")
-subdirs("../_deps/googletest-build")
